ef1a4519c599faf848e604a1771733a9f85e18b0
MSch33 <99237120+mdschenck@users.noreply.github.com>
2022-04-05 23:59:11 -0400

    added save to local storage when button clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,7 +51,7 @@ schedHours = [
   hour22,
 ];
 
-// schedHours = JSON.parse(localStorage.getItem("calendarList")) || [];
+schedHours = JSON.parse(localStorage.getItem("calendarList")) || [];
 localStorage.setItem("calendarList", JSON.stringify(schedHours));
 console.log(schedHours);
 
@@ -76,7 +76,7 @@ var loadCalendar = function (event) {
       schedHours[i].hour
     }</td><td class="editable col-10 ${rowColor}" contentEditable="true">${
       schedHours[i].task
-    }</td><td class="col-1"><button class="saveBtn btn save bi bi-save" type="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-save" viewBox="0 0 16 16">
+    }</td><td class="col-1"><button class="saveBtn btn save bi bi-save" type="button"><svg xmlns="http://www.w3.org/2000/svg" width="40" height="16" fill="currentColor" class="bi bi-save" viewBox="0 0 16 16">
     <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z"/>
   </svg></button></td><tr>
     `;
@@ -84,8 +84,11 @@ var loadCalendar = function (event) {
   }
 };
 
-// var saveTask = function (event) {};
-
 loadCalendar();
 
-// saveBtn.addEventListener("click", saveTask);
+var saveTask = function (event) {
+  localStorage.setItem("calendarList", JSON.stringify(schedHours));
+  console.log("Updated:" + schedHours);
+};
+
+saveBtn.addEventListener("click", saveTask);
